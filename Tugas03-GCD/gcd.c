7c8c7c6b054d0af5fc30ebdd94b134a0cc12558f
alstanlin <146912468+alstanlin@users.noreply.github.com>
2023-10-25 10:41:44 +0700

    Add files via upload

---
+++ Tugas03-GCD/gcd.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int a, int b) {
+    while(b != 0) {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int main(){
+    int a, b;
+    printf("Masukkan Nilai A: "); scanf("%d", &a);
+    printf("Masukkan Nilai B: "); scanf("%d", &b);
+
+if(a != 0 && b != 0){
+    a = abs(a);
+    b = abs(b);
+    printf("Nilai GCD adalah %d\n", a, b, gcd(a, b));
+    main();
+
+}else{
+    printf("Mohon Maaf Nilai Tidak Boleh Sama Dengan Nol (0)!\n");
+    main();
+    }
+}
