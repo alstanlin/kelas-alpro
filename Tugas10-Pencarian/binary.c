750a910c81d2ea27bdbf8ea02af36d8ad9cb2839
alstanlin <146912468+alstanlin@users.noreply.github.com>
2023-12-27 09:45:09 +0700

    Create binary.c

---
+++ Tugas10-Pencarian/binary.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define SIZE 100
+
+size_t linearSearch(const int array[], int key, size_t size);
+
+int main(void){
+    int a[SIZE];
+    for(size_t x=0; x<SIZE; x++){
+        a[x]=2*x+1;
+    }
+
+    printf("Tuliskan Bilangan yang Dicari: ");
+    int searchKey;
+    scanf("%d", &searchKey);
+
+    size_t index=linearSearch(a, searchKey, SIZE);
+
+    if(index!=-1){
+        printf("Menemukan Nilai pada Index %d\n", index);
+    }else{
+        puts("Nilai Tidak Ditemukan");
+    }
+}
